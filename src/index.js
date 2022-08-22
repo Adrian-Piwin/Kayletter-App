69c32b7173a49754db26d27073b5bb9e885bae34
Adrian Piwin <APiwin@mysysltd.com>
2022-08-22 16:58:35 -0400

    Created interface Can add notes to database associated with code Can load notes associated with code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore'
 
+var noteList = []
+
+window.addEventListener("load", function(event) {
+    let btnAddNote = document.getElementById("btnAddNote")
+    let btnFinish = document.getElementById("btnFinish")
+    let btnLoadNotes = document.getElementById("btnLoadNotes")
+
+    btnAddNote.addEventListener('click', addNote)
+    btnFinish.addEventListener('click', finish)
+    btnLoadNotes.addEventListener('click', loadNotes)
+});
+
 const firebaseApp = initializeApp({
     piKey: "AIzaSyD9RHiBSUHoxoBtKPC-sj4tBEn4RyPKZeo",
     authDomain: "luckygame-1905f.firebaseapp.com",
@@ -14,23 +26,31 @@ const firebaseApp = initializeApp({
 
 const db = getFirestore();
 
-const colRef = collection(db, 'safe-code');
+async function dbGetNotes(code){
+    const colRef = collection(db, code)
 
-getDocs(colRef)
-.then((snapshot) => {
+    const snapshot = await getDocs(colRef)
     let data = []
     snapshot.docs.forEach((doc) => {
         data.push({...doc.data(), id: doc.id})
     }) 
-    console.log(data);
-})
-.catch(err => {
-    console.log(err.message)
-})
 
-function addData(val){
+    return data
+}
+
+function dbAddNote(code, val){
+    const colRef = collection(db, code)
+
+    let currentDate = new Date()
+    let cDay = currentDate.getDate()
+    let cMonth = currentDate.getMonth() + 1
+    let cYear = currentDate.getFullYear()
+    let today = cDay + "/" + cMonth + "/" + cYear
+
     addDoc(colRef, {
-        code: val
+        note: val,
+        createdOn: today,
+        read: false
     });
 }
 
@@ -39,3 +59,51 @@ function deleteData(val){
 
     deleteDoc(docRef)
 }
+
+// Add note element on page
+function addNote(){
+    let noteContainer = document.getElementById("noteContainer");
+    let note = document.createElement("input")
+    note.classList.add("w3-input")
+    noteContainer.appendChild(note)
+}
+
+// Apply changes to the given code
+function finish(){
+    let code = document.getElementById("inputCode").value
+    let nodeContainer = document.getElementById("noteContainer")
+
+    // Add each note found in container to the database
+    for (let i = 0; i < nodeContainer.children.length; i++){
+        if (nodeContainer.children[i].value != "")
+            dbAddNote(code, nodeContainer.children[i].value)
+    }
+}
+
+// Load notes for this code 
+function loadNotes(){
+    let code = document.getElementById("inputCode").value
+    dbGetNotes(code).then(notes => {
+        let noteContainer = document.getElementById("noteContainer").children
+
+        for (let i = 0; i < notes.length; i++){
+            if (noteContainer[i].value != undefined) {
+                noteContainer[i].value = notes[i].note
+            }
+            else{
+                addNote()
+                noteContainer = document.getElementById("noteContainer").children
+                noteContainer[i].value = notes[i].note
+            }
+        }
+
+        noteList = notes
+    });
+
+    
+    
+}
+
+
+
+
