f9eed92bd773e5c940da983810ff4a5d3eecff1c
Adrian <adrianpiwin@gmail.com>
2022-09-22 10:17:06 -0400

    Separated static var to own page

---
+++ src/staticvariables.js
@@ -0,0 +1,9 @@
+
+// Strings to access variables from db
+export const dbVarPassword = "password"
+export const dbVarPageTitle = "pageTitle"
+export const dbVarImageURL = "imageURL"
+export const dbVarUsername = "displayPageId"
+export const dbVarFavorite = "favoriteNote"
+
+export const defaultImgSrc = "/flowers/sunflower.gif"
